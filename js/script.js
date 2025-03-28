64b45b0050192e3902abbe46bcd0d4ca5880db06
FelpoMS <felipemacielsierote@gmail.com>
2025-03-28 20:18:38 -0300

    v1.1
    melhora na reautilização do codigo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-function somar(){
-    
+function lerinput(){
     let numero1= parseFloat(document.getElementById('numero1').value);
     let numero2= parseFloat(document.getElementById('numero2').value);
+    
+    return{numero1,numero2};
+}
+
+function somar(){
+    
+    let {numero1,numero2}=lerinput();
     let resultado= numero1+numero2;
 
     document.getElementById('resultado').innerText=resultado;
@@ -9,8 +15,7 @@ function somar(){
 
 function subtrair(){
     
-    let numero1= parseFloat(document.getElementById('numero1').value;)
-    let numero2= parseFloat(document.getElementById('numero2').value;)
+    let {numero1,numero2}=lerinput();
     let resultado= numero1-numero2;
 
     document.getElementById('resultado').innerText=resultado;
@@ -18,14 +23,26 @@ function subtrair(){
 
 function multiplicar(){
     
-    let numero1= parseFloat(document.getElementById('numero1').value;)
-    let numero2= parseFloat(document.getElementById('numero2').value;)
-    
+    let {numero1,numero2}=lerinput();
+    let resultado= numero1*numero2;
+
+    document.getElementById('resultado').innerText=resultado;
 }
 
 function dividir(){
     
-    let numero1= parseFloat(document.getElementById('numero1').value;)
-    let numero2= parseFloat(document.getElementById('numero2').value;)
+    let {numero1,numero2}=lerinput();
     
+
+    document.getElementById('resultado').innerText=resultado;
+    if(numero2 === 0){
+        document.getElementById('resultado').innerText= "divisão por zero";
+    }else{
+        let resultado= numero1/numero2;
+        document.getElementById('resultado').innerText=resultado;
+    }
+}
+
+function clear(){
+
 }
