d6d7a9aa72d6b102cab67445bd261e4f74b22e91
FelpoMS <felipemacielsierote@gmail.com>
2025-03-27 22:23:48 -0300

    v1 calculadora

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function somar(){
+    
+    let numero1= parseFloat(document.getElementById('numero1').value;)
+    let numero2= parseFloat(document.getElementById('numero2').value;)
+    console.log(2 +numero1);
+    console.log(numero2);
+}
